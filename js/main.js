c6400b76a0d1b8edfa023a6222e3ddcb707e1bc5
tdhight <tonyahight@Tonyas-MacBook-Pro.local>
2017-03-25 15:20:39 -0400

    Links and flexbox
    Ensure links set up and set up other creations using flexbox

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ $(document).ready(function(){
 	  cssEase: 'linear'
 	});
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
